9a897b757804d5877d7c2b14589132f86860db71
likhita-810 <likhitakonathala@gmail.com>
2025-04-15 16:00:14 +0530

    variables and data types

---
+++ javascript/1-var-data-types.js
@@ -0,0 +1,38 @@
+console.log("lets get started with JS");
+var name = "likhi";
+console.log(name);
+var name = 'roshi'
+console.log(name);
+let fullName = "likhita";
+console.log(fullName);
+fullName = "likhita k"
+console.log(fullName);
+const pi = 3.24;
+console.log(pi);
+console.log(typeof(pi));
+const student = {
+    fullName : 'maya',
+    age : 20,
+    cgpa : 7.55,
+    isPass : true,
+}
+console.log(student);
+console.log(typeof(student));
+console.log(student.age);
+console.log(student['cgpa']);
+
+student["age"] = 25
+console.log(student.age);
+
+// practice code
+const product = {
+    item : "pen",
+    rating : 4,
+    offer: 5,
+    price: 270,
+    isDeal : true,
+}
+console.log(product);
+console.log(product.item);
+console.log(typeof(product));
+console.log(typeof(product.price));
